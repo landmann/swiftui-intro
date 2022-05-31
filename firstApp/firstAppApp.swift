1d7e5574487de308263eb29d3e905d0002f913bc
Nate Landman <nlandmanc@gmail.com>
2022-05-31 13:22:21 +0400

    Initial Commit

---
+++ firstApp/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  firstApp
+//
+//  Created by landmann on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
