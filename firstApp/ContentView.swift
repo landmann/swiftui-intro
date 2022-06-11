6233e50acd04533379b046153b22b668baeb68b2
Nate Landman <nlandmanc@gmail.com>
2022-06-11 13:31:58 +0100

    Not sure where this is going

--- firstApp/ContentView.swift
+++ firstApp/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var show = false
+    @State var viewState = CGSize.zero
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            VStack(spacing: 16) {
+                Text("Meng - 28% complete")
+                .font(.caption)
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.08)))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color(#colorLiteral(red: 0.7882352941, green: 0.8196078431, blue: 0.862745098, alpha: 1)))
+                    .cornerRadius(12)
+                    .shadow(color: Color.white, radius: 0, x: 0, y: 1)
+                MenuRow(title: "Account", icon: "gear")
+                MenuRow(title: "Billing", icon: "creditcard")
+                MenuRow(title: "Sign out", icon: "person.crop.circle")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(radius: 30)
+            .padding(.horizontal, 30)
+            .padding(.bottom, 30)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+        }
     }
 }
 
@@ -19,3 +49,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
